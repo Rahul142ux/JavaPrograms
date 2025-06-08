3a17fd0b405a0bb0c4623aa74edd95f74c0ee952
Your Name <rahul.raskar@amdocs.com>
2025-06-08 16:39:42 +0530

    RevStr

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,23 @@
+import  java.lang.String;
+public class ReverseString {
+
+    public static void main(String[] args) {
+        ReverseString rs=new ReverseString();
+                 String name="Raskar";
+        System.out.println(rs.reverseString(name));
+    }
+    public String reverseString(String name)
+    {
+        int len=name.length();
+        char ch;
+        String rev="";
+        System.out.println(len);
+        for(int i=0;i<len;i++)
+        {
+            ch=name.charAt(i);
+            rev=ch+rev;
+        }
+        return rev;
+    }
+
+}
